dedd97860c684b999b57e791bf220fadf4b6635f
jonaswahanik <wahanik.jonas@vashonsd.org>
2021-10-07 11:18:07 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println(greet("Bob"));
+    }
+
+    public static String greet(String person) {
+
+        return "hello " + person;
+    }
+}
